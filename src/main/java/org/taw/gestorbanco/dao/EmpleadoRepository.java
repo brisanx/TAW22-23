09e597fb187ec5d6493f56c464aea03017323cec
KalvyRaw <118884228+KalvyRaw@users.noreply.github.com>
2023-05-04 20:37:35 +0200

    Delete src directory

--- src/main/java/org/taw/gestorbanco/dao/EmpleadoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.taw.gestorbanco.dao;
-
-import org.taw.gestorbanco.entity.EmpleadoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepository extends JpaRepository<EmpleadoEntity, Integer> {
-}
--- src/main/java/org/taw/gestorbanco/dao/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.taw.gestorbanco.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.taw.gestorbanco.entity.UsuarioEntity;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
-}
